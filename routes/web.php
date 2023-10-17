610db400ca97aaf45db879e3db010c556f9adcb3
Alexander Hurtado <alexander.hurtado7170@gmail.com>
2023-10-17 12:09:07 -0500

    department delete

--- routes/web.php
+++ routes/web.php
@@ -34,4 +34,5 @@ Route::get('/municipios/{municipio}',[MunicipioController::class, 'edit'])->name
 Route::get('/departamentos',[DepartamentoController::class, 'index'])->name('departamentos.index');
 Route::post('/departamentos',[DepartamentoController::class, 'store'])->name('departamentos.store');
 Route::get('/departamentos/create',[DepartamentoController::class, 'create'])->name('departamentos.create');
+Route::delete('/departamentos/{departamento}',[DepartamentoController::class, 'destroy'])->name('departamentos.destroy');
 ?>
