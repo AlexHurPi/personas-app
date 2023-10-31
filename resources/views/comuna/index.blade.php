2a54de083bbd1eec952f335c5fc41109c2dffea4
Alexander Hurtado <alexander.hurtado7170@gmail.com>
2023-10-31 15:47:29 -0500

    Initial project

--- resources/views/comuna/index.blade.php
+++ resources/views/comuna/index.blade.php
@@ -20,8 +20,10 @@
       <a type="button" class="btn btn-link" href="{{ route('municipios.index')}}">Municipios</a>
       <a type="button" class="btn btn-link" href="{{ route('departamentos.index')}}">Departamentos</a>
       <a type="button" class="btn btn-link" href="{{ route('paises.index')}}">Paises</a>
-      <h1>Commune List</h1>
-    <a href="{{ route('comunas.create')}}" class="btn btn-success">Add</a>
+      <a type="button" class="btn btn-link" href="{{ route('dashboard')}}">Perfil cerrar sesion</a>
+
+      <h1>Commune List</h1>    
+      <a href="{{ route('comunas.create')}}" class="btn btn-success">Add</a>
     
     <table class="table">
         <thead>
