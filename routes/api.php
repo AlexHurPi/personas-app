b4ac3d01916805b7aeed2b724d94732a53a31eb4
Alexander Hurtado <alexander.hurtado7170@gmail.com>
2023-11-09 02:22:14 -0500

    API ComunaController

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\api\ComunaController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,5 +16,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    return $request->user();    
 });
+
+Route::post('/comunas',[ComunaController::class, 'store'])->name('comunas.store');
+Route::get('/comunas',[ComunaController::class, 'index'])->name('comunas');
+Route::delete('/comunas/{comuna}',[ComunaController::class, 'destroy'])->name('comunas.destroy');
+Route::get('/comunas/{comuna}',[ComunaController::class, 'show'])->name('comunas.show');
+Route::put('/comunas/{comuna}',[ComunaController::class, 'update'])->name('comunas.update');
+
+Route::get('/municipios',[MunicipioController::class, 'index'])->name('municipios');
