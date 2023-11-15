f1d4a595575b2bda1ad81a38196ba9c13441172d
Alexander Hurtado <alexander.hurtado7170@gmail.com>
2023-11-15 14:55:18 -0500

    ajustes

--- routes/api.php
+++ routes/api.php
@@ -19,8 +19,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();    
 });
 
-Route::post('/comunas',[ComunaController::class, 'store'])->name('comunas.store');
 Route::get('/comunas',[ComunaController::class, 'index'])->name('comunas');
+Route::post('/comunas',[ComunaController::class, 'store'])->name('comunas.store');
 Route::delete('/comunas/{comuna}',[ComunaController::class, 'destroy'])->name('comunas.destroy');
 Route::get('/comunas/{comuna}',[ComunaController::class, 'show'])->name('comunas.show');
 Route::put('/comunas/{comuna}',[ComunaController::class, 'update'])->name('comunas.update');
